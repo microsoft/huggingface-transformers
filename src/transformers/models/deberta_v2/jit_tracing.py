81969f9b7b16f3f34571f5f7f6dbc0c4d885f20d
harshithapv <havenka@microsoft.com>
2021-05-14 18:43:16 +0000

    remove old commented code

--- src/transformers/models/deberta_v2/jit_tracing.py
+++ src/transformers/models/deberta_v2/jit_tracing.py
@@ -7,28 +7,6 @@ Logging util
 """
 import os
 import torch
-'''
-def traceable(cls):
-  """ Decorator over customer functions
-      There is an issue for tracing customer python torch Function, using this decorator to work around it.
-      e.g.
-      @traceable
-      class MyOp(torch.autograd.Function):
-      xxx
-  """
-  class _Function(object):
-    @staticmethod
-    def apply(*args):
-      jit_trace = (os.getenv('JIT_TRACE', 'False').lower() == 'true')
-      if jit_trace:
-        return cls.forward(_Function, *args)
-      else:
-        return cls.apply(*args)
-    @staticmethod
-    def save_for_backward(*args):
-      pass
-  return _Function
-'''
 
 def traceable(cls):
   class _Function(object):
