c1497552631986b8b01d9329ba08fe0737b880d1
Jingyan Wang <jingywa@microsoft.com@orttrainingdev9.d32nl1ml4oruzj4qz3bqlggovf.px.internal.cloudapp.net>
2023-01-12 01:49:43 +0000

    Use torch 2 compile

--- examples/pytorch/text-classification/run_glue.py
+++ examples/pytorch/text-classification/run_glue.py
@@ -375,7 +375,8 @@ def main():
         use_auth_token=True if model_args.use_auth_token else None,
         ignore_mismatched_sizes=model_args.ignore_mismatched_sizes,
     )
-
+    model = torch.compile(model)
+    
     # Preprocessing the raw_datasets
     if data_args.task_name is not None:
         sentence1_key, sentence2_key = task_to_keys[data_args.task_name]
